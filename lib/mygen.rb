76f6a91d9d1c5f5d3b56f0473b4697763324d229
Kristian Rasmussen <me@krx.io>
2016-01-04 14:20:31 +0100

    sketching generator

--- lib/mygen.rb
+++ lib/mygen.rb
@@ -1,5 +1,14 @@
 require "mygen/version"
+require "erb"
 
 module Mygen
-  # Your code goes here...
+  def self.parse
+    # read template files
+    # eval generator rb for variables
+    # process all template files with generator bindings
+    files = Dir.glob("spec/fixtures/template/**")
+    erb = ERB.new(File.read(files[0]))
+    b = binding
+    erb.result b
+  end
 end
